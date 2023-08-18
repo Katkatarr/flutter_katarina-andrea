75d03f1d3435345abfd8e9a0d75511c7d4620ea9
Katkatarr <melodyofagatha@gmail.com>
2023-08-18 16:41:24 +0700

    jawaban_soal_prioritas_1

--- 03_Fundamental_Dart/praktikum/soal_prioritas_satu.dart
+++ 03_Fundamental_Dart/praktikum/soal_prioritas_satu.dart
@@ -1,4 +1,4 @@
-// Buatlah rumus keliling dan luas persegi dan persegi panjang dengan menggunakan bahasa pemrograman Dart
+//1. Buatlah rumus keliling dan luas persegi dan persegi panjang dengan menggunakan bahasa pemrograman Dart
 int panjang = 5;
 int lebar = 6;
 int sisi = 10;
@@ -16,4 +16,16 @@ void main(){
   //mencetak keliling dan luas persegi panjang
   print('keliling persegi panjang dengan panjang= $panjang dan lebar = $lebar adalah $kelPersegiPanjang');
   print('luas persegi panjang dengan  panjang= $panjang dan lebar = $lebar  adalah $luasPersegiPanjang');
+}
+
+
+//2. Implementasikan rumus keliling dan luas dari Lingkaran dengan menggunakan Bahasa Dart
+double r = 10;
+double pi = 3.14;
+double kelilingLingkaran = 2 * pi* r;
+double luasLingkaran = pi * r * r;
+
+void main(){
+  print('Keliling lingkaran dengan jari-jari = $r adalah $kelilingLingkaran');
+  print('Luas lingkaran dengan jari-jari = $r: adalah $luasLingkaran');
 }
