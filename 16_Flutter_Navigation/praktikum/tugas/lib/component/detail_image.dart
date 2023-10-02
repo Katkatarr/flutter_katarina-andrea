3341458bf63f78510d63f17cdcec5d9795adefbb
Katkatarr <melodyofagatha@gmail.com>
2023-10-02 11:31:08 +0700

    tugas_prioritas_eksplorasi

---
+++ 16_Flutter_Navigation/praktikum/tugas/lib/component/detail_image.dart
@@ -0,0 +1,24 @@
+import 'package:challenge/main.dart';
+import 'package:flutter/material.dart';
+
+class DetailImage extends StatelessWidget {
+  const DetailImage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Detail Image'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text('Ini adalah tampilan detail gambar.',
+                  style: TextStyle(fontSize: 24)),
+              MyApp.detailImage(),
+            ], // Perbaikan disini
+          ),
+        ));
+  }
+}
