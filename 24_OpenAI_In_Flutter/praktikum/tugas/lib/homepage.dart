8b68efab6fc240b5c43ee0d51793282387fd8517
Katkatarr <melodyofagatha@gmail.com>
2023-11-17 11:03:13 +0700

    tugas materi 24

--- 24_OpenAI_In_Flutter/praktikum/tugas/lib/homepage.dart
+++ 24_OpenAI_In_Flutter/praktikum/tugas/lib/homepage.dart
@@ -2,6 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
+class ChatMessage {
+  final String text;
+  final bool isUserMessage;
+
+  ChatMessage({
+    required this.text,
+    required this.isUserMessage,
+  });
+}
+
 class MyHomePage extends StatefulWidget {
   @override
   _MyHomePageState createState() => _MyHomePageState();
@@ -12,6 +22,7 @@ class _MyHomePageState extends State<MyHomePage> {
   TextEditingController cameraController = TextEditingController();
   TextEditingController storageController = TextEditingController();
   String recommendation = '';
+  List<ChatMessage> messages = []; // Define a list to store chat messages
 
   @override
   Widget build(BuildContext context) {
@@ -51,6 +62,24 @@ class _MyHomePageState extends State<MyHomePage> {
               'Rekomendasi: $recommendation',
               style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
             ),
+            SizedBox(height: 20),
+            Expanded(
+              child: ListView.builder(
+                itemCount: messages.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text(
+                      messages[index].text,
+                      style: TextStyle(
+                        color: messages[index].isUserMessage
+                            ? Colors.blue
+                            : Colors.green,
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
           ],
         ),
       ),
@@ -58,16 +87,10 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   void getRecommendation() async {
-    String key = '';
-    final apiUrl =
-        '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'; // Ganti dengan URL API OpenAI Anda
-    final apiKey = key; // Ganti dengan kunci API OpenAI Anda
-
-    final Map<String, dynamic> data = {
-      'price': priceController.text,
-      'camera': cameraController.text,
-      'storage': storageController.text,
-    };
+    String apiKey =
+        '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'; // Replace with your OpenAI API key
+    String engineId = 'text-davinci-003'; // Replace with the specific engine ID
+    String apiUrl = 'https://api.openai.com/v1/engines/$engineId/completions';
 
     final response = await http.post(
       Uri.parse(apiUrl),
@@ -77,27 +100,29 @@ class _MyHomePageState extends State<MyHomePage> {
         'Authorization': 'Bearer $apiKey',
       },
       body: jsonEncode(<String, dynamic>{
-        "model": "text-davinci-003",
-        'prompt': "Sebagai seorang ahli dalam merekomendasikan smartphone, saya, [Nama Anda], akan memberikan rekomendasi smartphone berdasarkan kebutuhan Anda. Berikan saya informasi berikut:\n\n" +
+        "prompt": "Sebagai seorang ahli dalam merekomendasikan smartphone, saya, [Nama Anda], akan memberikan rekomendasi smartphone berdasarkan kebutuhan Anda. Berikan saya informasi berikut:\n\n" +
             "1. Budget Anda: ${priceController.text} IDR\n" +
             "2. Kebutuhan Kamera (MP): ${cameraController.text} MP\n" +
             "3. Kapasitas Penyimpanan Internal: ${storageController.text} GB\n\n" +
-            "Dengan informasi ini, saya akan memberikan rekomendasi smartphone yang sesuai untuk Anda.",
-
-        // 'prompt':
-        //     "kamu sekarang adalah search engginer yang bertugas memberikan referensi buku berdasarkan inputan user" +
-        //         "data user akan didapatkan dari variabel berikut $userMessage" +
-        //         "berikan referensi berdasarkan 1\n2\n\n3.\noutput yang ingin saya hasilkan hanya berupa title dari setiap bukunya" +
-        //         "example output json data {title : judul 1 autor : author 1, }",
-        'max_tokens': 100,
+            "Dengan informasi ini, saya akan memberikan rekomendasi smartphone yang sesuai untuk Anda. [Input budget: ${priceController.text}, camera: ${cameraController.text}, storage: ${storageController.text}]",
+        'max_tokens': 500,
       }),
-      //body: jsonEncode(data),
     );
 
     if (response.statusCode == 200) {
-      final result = jsonDecode(response.body);
+      final responseData = jsonDecode(response.body);
+      print('Respon sukses: ${responseData['choices'][0]['text']}');
       setState(() {
-        recommendation = result['recommendation'];
+        recommendation = responseData['choices'][0]['text'];
+        messages.add(ChatMessage(
+          text:
+              'User: [Input budget: ${priceController.text}, camera: ${cameraController.text}, storage: ${storageController.text}]',
+          isUserMessage: true,
+        ));
+        messages.add(ChatMessage(
+          text: 'AI: $recommendation',
+          isUserMessage: false,
+        ));
       });
     } else {
       setState(() {
